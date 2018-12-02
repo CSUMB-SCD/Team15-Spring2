2bd8ba418809c1584695b43535b14eeffe97ba72
Manjit Gurcha <mgurcha@csumb.edu>
2018-12-01 21:15:49 -0800

    trying to fix heroku

--- src/main/java/edu/csumb/cst438/team15/usersdb/entities/User.java
+++ src/main/java/edu/csumb/cst438/team15/usersdb/entities/User.java
@@ -3,7 +3,7 @@ package edu.csumb.cst438.team15.usersdb.entities;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "Users")
+@Document(collection = "User")
 public class User {
     @Id
     private String username;
