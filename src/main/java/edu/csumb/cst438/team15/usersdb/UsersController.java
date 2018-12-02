37ed4b7450c4a62d69e308249c6cbc30604a45f1
Manjit Gurcha <mgurcha@csumb.edu>
2018-12-01 21:18:37 -0800

    trying to fix heroku

--- src/main/java/edu/csumb/cst438/team15/usersdb/UsersController.java
+++ src/main/java/edu/csumb/cst438/team15/usersdb/UsersController.java
@@ -1,9 +1,10 @@
 package edu.csumb.cst438.team15.usersdb;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.csumb.cst438.team15.usersdb.entities.User;
@@ -13,7 +14,8 @@ public class UsersController {
     @Autowired
     IUsersRepository usersRepository;
 
-    @GetMapping("/allUsers")
+    @CrossOrigin(origins = "*") 
+    @RequestMapping("/allUsers")
     public List<User> getAll() {
         List<User> result = usersRepository.findAll();
         return result;
